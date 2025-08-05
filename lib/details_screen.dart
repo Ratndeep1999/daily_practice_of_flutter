63c29df6e06df4a92b7076ebfaa03ea86b5e2b1b
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-05 20:53:38 +0530

    details_screen.dart : data received through list item

--- lib/details_screen.dart
+++ lib/details_screen.dart
@@ -10,6 +10,35 @@ class DetailsScreen extends StatefulWidget {
 class _DetailsScreenState extends State<DetailsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final Map getData = ModalRoute.of(context)?.settings.arguments as Map;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('List Screen', style: TextStyle(color: Colors.black)),
+        centerTitle: true,
+        backgroundColor: Colors.tealAccent,
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Title : ${getData['title']}',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Description : ${getData['description']}',
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Price : \$${getData['price']}',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
