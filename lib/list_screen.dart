678bc195bc3c3d68f1d0777903498c4fd906cb86
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-05 20:52:54 +0530

    list_screen.dart : list item 3 add and arguments pass

--- lib/list_screen.dart
+++ lib/list_screen.dart
@@ -1,3 +1,4 @@
+import 'package:daily_practice/routes.dart';
 import 'package:flutter/material.dart';
 
 class ListScreen extends StatefulWidget {
@@ -8,11 +9,183 @@ class ListScreen extends StatefulWidget {
 }
 
 class _ListScreenState extends State<ListScreen> {
+  Map<String, dynamic> dataToPass = {
+    'title': '',
+    'description': '',
+    'price': 0,
+  };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: SafeArea(child: Column()),
+      appBar: AppBar(
+        title: Text('List Screen', style: TextStyle(color: Colors.black)),
+        centerTitle: true,
+        backgroundColor: Colors.tealAccent,
+      ),
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.all(20),
+          children: <Widget>[
+            InkWell(
+              onTap: () {
+                Navigator.pushNamed(
+                  context,
+                  kDetailsPage,
+                  arguments:
+                      dataToPass = {
+                        'title': 'Item 1',
+                        'description': 'This is list Item 1',
+                        'price': 700,
+                      },
+                );
+              },
+              child: Container(
+                height: 100,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.only(top: 10, bottom: 10),
+                decoration: BoxDecoration(
+                  color: Colors.red.shade100,
+                  borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(50),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Title : Item 1',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'Description : This is the description of Item 1',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 15,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          'Price : \$700',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {
+                Navigator.pushNamed(
+                  context,
+                  kDetailsPage,
+                  arguments:
+                  dataToPass = {
+                    'title': 'Item 2',
+                    'description': 'This is list Item 2',
+                    'price': 800,
+                  },
+                );
+              },
+              child: Container(
+                height: 100,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.only(top: 10, bottom: 10),
+                decoration: BoxDecoration(
+                  color: Colors.red.shade100,
+                  borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(50),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Title : Item 2',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'Description : This is the description of Item 2',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 15,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          'Price : \$800',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {
+                Navigator.pushNamed(
+                  context,
+                  kDetailsPage,
+                  arguments:
+                  dataToPass = {
+                    'title': 'Item 3',
+                    'description': 'This is list Item 3',
+                    'price': 900,
+                  },
+                );
+              },
+              child: Container(
+                height: 100,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.only(top: 10, bottom: 10),
+                decoration: BoxDecoration(
+                  color: Colors.red.shade100,
+                  borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(50),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Title : Item 3',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'Description : This is the description of Item 3',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 15,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          'Price : \$900',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
