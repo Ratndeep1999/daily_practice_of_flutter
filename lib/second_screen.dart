91a6def16829067677e2e2e0c7a1ec0c9f8725f2
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-22 12:38:24 +0530

    second screen with pop and push navigation

--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -1,3 +1,4 @@
+import 'package:daily_practice/routes/named_routes.dart';
 import 'package:flutter/material.dart';
 
 class SecondScreen extends StatefulWidget {
@@ -10,6 +11,51 @@ class SecondScreen extends StatefulWidget {
 class _SecondScreenState extends State<SecondScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Second Screen',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+              ),
+              SizedBox(height: 20,),
+              // push 
+              IconButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed(kThirdScreen);
+                },
+                highlightColor: Colors.transparent,
+                splashRadius: 0.50,
+                icon: Icon(
+                  Icons.arrow_right_alt_rounded,
+                  size: 80,
+                  color: Colors.black45,
+                ),
+              ),
+              SizedBox(height: 5,),
+              // pop
+              RotatedBox(
+                quarterTurns: 2,
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  highlightColor: Colors.transparent,
+                  splashRadius: 0.50,
+                  icon: Icon(
+                    Icons.arrow_right_alt_rounded,
+                    size: 80,
+                    color: Colors.black45,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
