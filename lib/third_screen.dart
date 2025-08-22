c02e88409e11d126a0cb804e3000f36a513334b6
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-22 13:31:51 +0530

    all lifecycle methods testing when and how to call

--- lib/third_screen.dart
+++ lib/third_screen.dart
@@ -10,8 +10,47 @@ class ThirdScreen extends StatefulWidget {
 }
 
 class _ThirdScreenState extends State<ThirdScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    debugPrint('initState method...');
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    debugPrint('did_change_dependencies method...');
+  }
+
+  @override
+  void didUpdateWidget(covariant ThirdScreen oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    debugPrint('did_update_widget method...');
+  }
+
+  @override
+  void setState(VoidCallback fn) {
+    super.setState(fn);
+    debugPrint('set_state method...');
+  }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+    debugPrint('deactivate method...');
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    debugPrint('dispose method...');
+  }
+
   @override
   Widget build(BuildContext context) {
+    debugPrint('Build method...');
+
     return Scaffold(
       body: SafeArea(
         child: Padding(
