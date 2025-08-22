783f73d76cec290fdde12d187785076ec8b05f6d
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-22 12:53:19 +0530

    third screen with listview items of random colors and items

--- lib/third_screen.dart
+++ lib/third_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 class ThirdScreen extends StatefulWidget {
@@ -10,6 +11,68 @@ class ThirdScreen extends StatefulWidget {
 class _ThirdScreenState extends State<ThirdScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Third Screen',
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+                  ),
+                  SizedBox(width: 40),
+                  InkWell(
+                    onTap: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Icon(Icons.arrow_back, size: 30),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: 50,
+                  itemBuilder: (BuildContext context, int index) {
+                    return SizedBox(
+                      height: 50,
+                      width: MediaQuery
+                          .of(context)
+                          .size
+                          .width,
+                      child: DecoratedBox(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.white),
+                          color: _randomColor(),
+                        ),
+                        child: Center(
+                          child: Text(
+                            '$index',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  // method to create random color
+  Color _randomColor() {
+    Color randomColor = Color(Random().nextInt(0xffffff));
+    return randomColor;
   }
 }
