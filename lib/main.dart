4362284f770698343d75e92bd5733852ba3e7e0d
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-27 20:32:08 +0530

    image, elevated button, visibility, network image

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
       //         randomIndex: ModalRoute.of(context)?.settings.arguments as int,
       //       ),
       // },
-      home: Practice(),
+      home: ImageVisibility(),
     );
   }
 }
