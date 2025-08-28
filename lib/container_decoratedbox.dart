2f20a2dc38d22f2e35763f86ab5082e98ce0173e
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-28 19:17:51 +0530

    container and decorated box

--- lib/container_decoratedbox.dart
+++ lib/container_decoratedbox.dart
@@ -12,22 +12,35 @@ class ContainerDecoratedbox extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
-              margin: EdgeInsets.only(left: 80, bottom: 80),  // outer space
-              padding: EdgeInsets.symmetric(horizontal: 60, vertical: 50),  // inner space
+              // outer space
+              margin: EdgeInsets.only(left: 140, bottom: 80),
+              // inner space
+              padding: EdgeInsets.symmetric(horizontal: 60, vertical: 50),
               height: 250,
               width: 250,
-              alignment: Alignment.bottomRight,  // to align child widget
+              // pass min/max height and width
+              constraints: BoxConstraints(),
+              // to align child widget
+              alignment: Alignment.bottomRight,
+              // rotate the container
+              transform: Matrix4.rotationZ(0.4),
               decoration: BoxDecoration(color: Colors.greenAccent.shade200),
               child: Container(
                 height: 100,
                 width: 100,
+                foregroundDecoration: BoxDecoration(
+                  // inner decoration
+                  shape: BoxShape.circle,
+                  color: Colors.black,
+                  gradient: LinearGradient(
+                    colors: [Colors.red, Colors.deepOrange, Colors.yellow],
+                  ),
+                ),
                 decoration: BoxDecoration(color: Colors.purple.shade500),
               ),
             ),
 
-            DecoratedBox(
-              decoration: BoxDecoration(),
-            ),
+            DecoratedBox(decoration: BoxDecoration()),
           ],
         ),
       ),
