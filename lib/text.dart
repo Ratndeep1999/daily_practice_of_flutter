1a55d26a1e2b05476bf4bd5ebd0ad43bf5538a0f
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-29 11:11:45 +0530

    text widget

--- lib/text.dart
+++ lib/text.dart
@@ -5,6 +5,17 @@ class TextWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Text(
+              'Good Morning',
+              style: TextStyle(color: Colors.lightGreen, fontSize: 40),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
