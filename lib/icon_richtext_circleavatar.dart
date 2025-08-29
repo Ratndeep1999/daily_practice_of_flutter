1b3c98258bf403cd59ca99aafd0e8a09e5f58b3d
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-29 10:09:50 +0530

    icon, circle-avatar and rich-text

--- lib/icon_richtext_circleavatar.dart
+++ lib/icon_richtext_circleavatar.dart
@@ -7,20 +7,29 @@ class IconRichtextCircleavatar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Center(
-                  child: CircleAvatar(
-                    backgroundColor: Colors.yellowAccent,
-                    maxRadius: 130,
-                    // color of child content
-                    foregroundColor: Colors.red,
-                    child: Icon(Icons.house, size: 100,),
-                  )
-              )
-            ],
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundColor: Colors.yellowAccent,
+                maxRadius: 130,
+                // color of child content
+                foregroundColor: Colors.red,
+                minRadius: 50,
+                //backgroundImage: AssetImage('assets/images/demo.webp'),
+                //foregroundImage: AssetImage('assets/images/demo.webp'),
+                child: Icon(Icons.house, size: 100),
+                backgroundImage: NetworkImage(
+                  'https://tse2.mm.bing.net/th/id/OIP.9Hsj4_O03n2vX0Bcy3-u-gHaEo?rs=1&pid=ImgDetMain&o=7&rm=3',
+                ),
+                onBackgroundImageError: (error, stackTrace) {
+                  print("Image failed to load: $error");
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
