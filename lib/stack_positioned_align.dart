21f1effb7f58931fb81fd938bae8b2466e8f335a
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-30 13:20:11 +0530

    images, stack, positioned, align etc practice complete

--- lib/stack_positioned_align.dart
+++ lib/stack_positioned_align.dart
@@ -15,20 +15,20 @@ class StackPositionedAlign extends StatelessWidget {
               height: 200,
               width: double.infinity,
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(50.0),
-                  bottomRight: Radius.circular(50.0),
-                ),
+                // borderRadius: BorderRadius.only(
+                //   bottomLeft: Radius.circular(50.0),
+                //   bottomRight: Radius.circular(50.0),
+                // ),
                 // gradient: LinearGradient(
                 //   colors: [Color(0xff00ff87), Color(0xff60efff)],
                 // ),
               ),
               //clipBehavior: Clip.antiAlias,
               child: ClipRRect(
-                borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(50.0),
-                  bottomLeft: Radius.circular(50.0),
-                ),
+                // borderRadius: BorderRadius.only(
+                //   bottomRight: Radius.circular(50.0),
+                //   bottomLeft: Radius.circular(50.0),
+                // ),
                 clipBehavior: Clip.antiAlias,
                 child: Image.asset(
                   'assets/images/horizontal_pic3.jpg',
@@ -38,45 +38,45 @@ class StackPositionedAlign extends StatelessWidget {
             ),
 
             /// circular image
-            // Align(
-            //   alignment: Alignment(-0.8, -0.45),
-            //   child: CircleAvatar(
-            //     radius: 65,
-            //     backgroundImage: AssetImage('assets/images/square_pic.jpg'),
-            //   ),
-            // ),
-
-            /// vertical image
             Align(
-              alignment: Alignment(0.6, -0.4),
-              child: Container(
-                height: 280,
-                width: 180,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30.0),
-                  // gradient: LinearGradient(
-                  //   begin: Alignment.topCenter,
-                  //   end: Alignment.bottomCenter,
-                  //   colors: [Color(0xffe9b7ce), Color(0xffd3f3f1)],
-                  // ),
-                  // border: Border.all(color: Colors.black12, width: 0.5),
-                  // boxShadow: [
-                  //   BoxShadow(
-                  //     color: Colors.black12,
-                  //     blurRadius: 3,
-                  //     spreadRadius: 2,
-                  //   ),
-                  // ],
-                ),
-                // strict to follow border of child
-                clipBehavior: Clip.antiAlias,
-                child: Image.asset(
-                  'assets/images/vertical_pic.jpg',
-                  fit: BoxFit.cover,
-                  //isAntiAlias: false,
-                ),
+              alignment: Alignment(-0.6, -0.45),
+              child: CircleAvatar(
+                radius: 90,
+                backgroundImage: AssetImage('assets/images/square_pic2.jpg'),
               ),
             ),
+
+            /// vertical image
+            // Align(
+            //   alignment: Alignment(0.6, -0.4),
+            //   child: Container(
+            //     height: 280,
+            //     width: 180,
+            //     decoration: BoxDecoration(
+            //       borderRadius: BorderRadius.circular(30.0),
+            //       // gradient: LinearGradient(
+            //       //   begin: Alignment.topCenter,
+            //       //   end: Alignment.bottomCenter,
+            //       //   colors: [Color(0xffe9b7ce), Color(0xffd3f3f1)],
+            //       // ),
+            //       // border: Border.all(color: Colors.black12, width: 0.5),
+            //       // boxShadow: [
+            //       //   BoxShadow(
+            //       //     color: Colors.black12,
+            //       //     blurRadius: 3,
+            //       //     spreadRadius: 2,
+            //       //   ),
+            //       // ],
+            //     ),
+            //     // strict to follow border of child
+            //     clipBehavior: Clip.antiAlias,
+            //     child: Image.asset(
+            //       'assets/images/vertical_pic.jpg',
+            //       fit: BoxFit.cover,
+            //       //isAntiAlias: false,
+            //     ),
+            //   ),
+            // ),
           ],
         ),
       ),
