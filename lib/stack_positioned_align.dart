a16ce672a9565fe17032d97303172190a2605b24
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-30 11:52:02 +0530

    stack, positioned and align

--- lib/stack_positioned_align.dart
+++ lib/stack_positioned_align.dart
@@ -7,59 +7,31 @@ class StackPositionedAlign extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          //mainAxisAlignment: MainAxisAlignment.center,
+        child: Stack(
+          alignment: Alignment(-0.85, 5),
           children: [
-            Stack(
-              children: [
-                SizedBox(
-                  height: 200,
-                  width: double.infinity,
-                  child: DecoratedBox(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: [Color(0xff00ff87), Color(0xff60efff)],
-                      ),
-                    ),
+            SizedBox(
+              height: 180,
+              width: double.infinity,
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Color(0xff00ff87), Color(0xff60efff)],
                   ),
                 ),
-              ],
+              ),
             ),
 
-            // Center(
-            //   child: Stack(
-            //     //alignment: Alignment(0, 0),   // right/left  // up/down
-            //     alignment: Alignment.center,
-            //     fit: StackFit.passthrough,
-            //     children: [
-            //       Container(
-            //         height: 250,
-            //         width: 250,
-            //         color: Colors.purpleAccent,
-            //       ),
-            //       // this never moves
-            //       Container(height: 170, width: 170, color: Colors.orange),
-            //       Positioned(
-            //         top: 20,
-            //         right: 20,
-            //         child: Container(
-            //           height: 120,
-            //           width: 120,
-            //           color: Colors.yellowAccent,
-            //         ),
-            //       ),
-            //
-            //       Align(
-            //         alignment: Alignment(0.5, 0.3),
-            //         child: Container(
-            //           height: 70,
-            //           width: 70,
-            //           color: Colors.green,
-            //         ),
-            //       ),
-            //     ],
-            //   ),
-            // ),
+            CircleAvatar(
+              radius: 70,
+              backgroundImage: AssetImage('assets/images/demo.webp'),
+            ),
+
+            SizedBox(
+              height: 20,
+              width: 20,
+              child: DecoratedBox(decoration: BoxDecoration()),
+            ),
           ],
         ),
       ),
