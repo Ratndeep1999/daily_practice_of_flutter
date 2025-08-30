27911fb6a66f003a1f52018b500298056a8ad7ee
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-30 09:52:58 +0530

    stack, positioned and align

--- lib/stack_positioned_align.dart
+++ lib/stack_positioned_align.dart
@@ -7,8 +7,39 @@ class StackPositionedAlign extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Stack(
+                // alignment: Alignment(0.1, 3),   // right/left  // up/down
+                children: [
+                  Container(
+                    height: 250,
+                    width: 250,
+                    color: Colors.purpleAccent,
+                  ),
+                  // this never moves
+                  Positioned(
+                    top: 50,
+                    left: 50,
+                    right: 20,
+                    bottom: 20,
+                    child: Container(
+                      height: 170,
+                      width: 170,
+                      color: Colors.orange,
+                    ),
+                  ),
+                  Container(
+                    height: 120,
+                    width: 120,
+                    color: Colors.yellowAccent,
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
